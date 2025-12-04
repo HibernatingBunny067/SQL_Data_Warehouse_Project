7bc2d5450e70dce42a9f8d1571c0ff2f4d72b4d4
HibernatingBunny067 <harikeshv630@gmail.com>
2025-12-04 14:01:37 +0530

    Create set_config.sql for MySQL server settings
    This SQL script sets up environment settings for a local MySQL server, allowing for data import.

---
+++ scripts/set_config.sql
@@ -0,0 +1,12 @@
+/*
+=====================================================================================
+Setting up the environment settings on MySQL server running locally on my machine 
+=====================================================================================
+Purpose: 
+  - this file will contain server settings that maybe turned on or off depending on the need of the project
+WARNING:
+  - code maybe added as per needed and comments will indicate how to run those code snippets for optimum results 
+*/
+
+-- Run once per session in the server, allows for importing data from local to the locally run sql server directly
+SET GLOBAL local_infile = 1;
