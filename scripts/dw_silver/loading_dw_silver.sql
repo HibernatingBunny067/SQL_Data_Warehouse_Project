1714629c4ec758ea76725c225ae99acd90078bc3
HibernatingBunny067 <harikeshv630@gmail.com>
2025-12-05 19:21:21 +0530

    Add documentation and procedure call in loading_dw_silver.sql
    Added a comment block explaining the purpose of the SQL script and included a call to the stored procedure.

---
+++ scripts/dw_silver/loading_dw_silver.sql
@@ -0,0 +1,8 @@
+/*
+================================
+Filling in the dw_silver tables
+================================
+Purpose:
+  - a stored procedure call to populate the dw_silver tables
+*/
+CALL load_dw_silver();
