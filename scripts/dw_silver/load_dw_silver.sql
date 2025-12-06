51fb1b1e5e4b8248059556674145b3f5a617545f
HibernatingBunny067 <harikeshv630@gmail.com>
2025-12-06 15:58:53 +0530

    Refactor SQL insert for crm_sales_details table
    added correct logic for inserting in dw_silver sale details table

--- scripts/dw_silver/load_dw_silver.sql
+++ scripts/dw_silver/load_dw_silver.sql
@@ -85,38 +85,41 @@ FROM dw_bronze.crm_prd_info;
 -- Inserting Third Table
 TRUNCATE TABLE dw_silver.crm_sales_details;
 INSERT INTO dw_silver.crm_sales_details(
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
     sls_order_dt,
     sls_ship_dt,
     sls_due_dt,
     sls_sales,
-    sls_price
+    sls_quantity, 
+    sls_price     
 )
 SELECT 
-sls_ord_num,
-sls_prd_key,
-sls_cust_id,
--- checking valid dates
-CASE WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt) != 8 THEN NULL
-	ELSE CAST(CAST(sls_order_dt AS CHAR) AS DATE)
-END AS sls_order_dt,
-CASE WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt) != 8 THEN NULL
-	ELSE CAST(CAST(sls_ship_dt AS CHAR) AS DATE)
-END AS sls_ship_dt,
-CASE WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt) != 8 THEN NULL
-	ELSE CAST(CAST(sls_due_dt AS CHAR) AS DATE)
-END AS sls_due_dt,
--- checking for valid sales, prices and quantity
-CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
-		THEN sls_quantity*ABS(sls_price)
-	ELSE sls_sales
-END AS sls_sales,
-CASE WHEN sls_price IS NULL OR sls_price <= 0
-		THEN sls_sales / NULLIF(sls_quantity, 0)
-	ELSE sls_price 
-END AS sls_price
+    sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    -- checking valid dates
+    CASE WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt) != 8 THEN NULL
+        ELSE CAST(CAST(sls_order_dt AS CHAR) AS DATE)
+    END AS sls_order_dt,
+    CASE WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt) != 8 THEN NULL
+        ELSE CAST(CAST(sls_ship_dt AS CHAR) AS DATE)
+    END AS sls_ship_dt,
+    CASE WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt) != 8 THEN NULL
+        ELSE CAST(CAST(sls_due_dt AS CHAR) AS DATE)
+    END AS sls_due_dt,
+    
+    -- checking for valid sales
+    CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+            THEN sls_quantity * ABS(sls_price)
+        ELSE sls_sales
+    END AS sls_sales,
+    sls_quantity, 
+    CASE WHEN sls_price IS NULL OR sls_price <= 0
+            THEN sls_sales / NULLIF(sls_quantity, 0)
+        ELSE sls_price 
+    END AS sls_price
 FROM dw_bronze.crm_sales_details;
 -- Inserting into ERP Table 1
 SELECT '>> dw_silver.erp_cust_az12';
