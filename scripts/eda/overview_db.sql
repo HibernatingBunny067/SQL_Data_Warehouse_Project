b5cffc6f6e86fb7e5c66898a9d4593e05026e20b
HibernatingBunny067 <harikeshv630@gmail.com>
2025-12-06 20:00:53 +0530

    Update overview_db.sql with additional script note
    Added a note about a second script that prints table columns.

--- scripts/eda/overview_db.sql
+++ scripts/eda/overview_db.sql
@@ -4,7 +4,11 @@ Script to get an overview of the database
 ================================================
 Purpose:
   - to check the metadata of the warehouse database with all the tables in them
-
+  - second script prints the tables with all the columns present 
 */
+
 SELECT * FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA IN ('dw_gold','dw_silver','dw_bronze');
+
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA IN ('dw_gold','dw_silver','dw_bronze');
